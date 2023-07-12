9a796c0e6c7b1925de87a86d54edea09fe0298a2
hopkins385 <stadhouders.sven@gmail.com>
2023-07-12 10:41:04 +0200

    chat gui & llmacpp values

--- main.py
+++ main.py
@@ -2,29 +2,50 @@ import os
 
 import streamlit as st
 from dotenv import load_dotenv
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.chains import LLMChain
+from langchain import LLMChain, PromptTemplate
+from langchain.callbacks.base import BaseCallbackHandler
 from langchain.llms import LlamaCpp
-from langchain.prompts import PromptTemplate
-from streamlit_chat import message
+from langchain.schema import ChatMessage
 
-callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
-
-load_dotenv(".env_file")
+load_dotenv()
 
 
-def load_chain():
-    """Logic for loading the chain you want to use should go here."""
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+
+if "system_message" not in st.session_state:
+    st.session_state[
+        "system_message"
+    ] = "You are an AI assistant that follows instruction extremely well. Help as much as you can. If the user speaks German, please also answer in German."
+
+st.text_input("System Message", key="system_message")
+
+st.chat_message("assistant").write("Wie kann ich helfen?")
+
+if "chain" not in st.session_state:
+    stream_handler = StreamHandler(st.empty())
     llm = LlamaCpp(
         model_path=os.environ.get(
             "LLM_MODEL_PATH", "./orca-mini-v2-ger-7b.ggmlv3.q4_0.bin"
         ),
-        callback_manager=callback_manager,
-        verbose=True,
+        callbacks=[stream_handler],
+        verbose=False,
         n_gpu_layers=1,
-        # n_batch=512,
-    )
+        n_ctx=2048,
+        use_mlock=True,
+        temperature=0.7,
+        top_p=1.0,
+        top_k=50,
+        max_tokens=1024,
+        repeat_penalty=1.1,
+    )  # type: ignore
     template = "### System:\n{system}\n\n### User:\n{instruction}\n\n### Response:\n"
     prompt = PromptTemplate(
         template=template, input_variables=["system", "instruction"]
@@ -32,37 +53,68 @@ def load_chain():
     prompt_w_system = prompt.partial(
         system="You are an AI assistant that follows instruction extremely well. Help as much as you can. If the user speaks German, please also answer in German."
     )
-    chain = LLMChain(prompt=prompt_w_system, llm=llm)
-    return chain
+    llm_chain = LLMChain(prompt=prompt_w_system, llm=llm)
+    st.session_state["chain"] = llm_chain
 
 
-chain = load_chain()
-
-# From here down is all the StreamLit UI.
-st.set_page_config(page_title="Demo", page_icon=":robot:")
-st.header("Demo")
-
-if "generated" not in st.session_state:
-    st.session_state["generated"] = []
-
-if "past" not in st.session_state:
-    st.session_state["past"] = []
+def update_model_parameters(temperature, top_p, top_k, repeat_penalty, max_tokens):
+    st.session_state["chain"].llm.temperature = temperature
+    st.session_state["chain"].llm.top_p = top_p
+    st.session_state["chain"].llm.top_k = top_k
+    st.session_state["chain"].llm.repeat_penalty = repeat_penalty
+    st.session_state["chain"].llm.max_tokens = max_tokens
 
 
-def get_text():
-    input_text = st.text_input("You: ", "Hallo, wie gehts?", key="input")
-    return input_text
+with st.sidebar:
+    temperature = st.slider(
+        "Temperature",
+        min_value=0.0,
+        max_value=1.0,
+        value=0.7,
+        step=0.1,
+        key="temperature",
+    )
+    top_p = st.slider(
+        "top_p", min_value=0.5, max_value=1.0, value=1.0, step=0.05, key="top_p"
+    )
+    top_k = st.slider(
+        "top_k", min_value=10, max_value=80, value=50, step=10, key="top_k"
+    )
+    repeat_penalty = st.slider(
+        "repeat_penalty",
+        min_value=0.0,
+        max_value=2.0,
+        value=1.1,
+        step=0.1,
+        key="repeat_penalty",
+    )
+    max_tokens = st.slider(
+        "max_tokens",
+        min_value=128,
+        max_value=1024,
+        value=1024,
+        step=128,
+        key="max_tokens",
+    )
+    st.button(
+        "Update Model Parameter",
+        key="update",
+        on_click=update_model_parameters(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            repeat_penalty=repeat_penalty,
+            max_tokens=max_tokens,
+        ),
+    )
 
+# input_placeholder=st.empty()
 
-user_input = get_text()
+if prompt := st.chat_input():
+    st.chat_message("user").write(prompt)
 
-if user_input:
-    output = chain.run(instruction=user_input)
-
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-if st.session_state["generated"]:
-    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+    with st.chat_message("assistant"):
+        stream_handler = StreamHandler(st.empty())
+        st.session_state["chain"].llm.callbacks[0] = stream_handler
+        response = st.session_state["chain"].run(prompt)
+        # st.chat_message('Assistant').write(response)
---
+++ readme.md
@@ -0,0 +1,7 @@
+# orca-mini-v2-ger
+
+## How to
+
+1. run `pip install -r requirements.txt`
+2. configure your model-path in the `.env` file
+3. execute `streamlit run main.py`
